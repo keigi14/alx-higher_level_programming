9fe1e0b39e5d0c69543ced1cb6d5f6871e996ed4
keigi14 <kbndbrn5@gmail.com>
2024-01-03 12:48:09 -0800

    the list flile written wrong

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -3,15 +3,15 @@
 
 #include <stdlib.h>
 /**
- * struct listint_s - singly linked list init.
+ * struct listint_s - singly linked list
  * @n: int
- * @next: point to de next node
- * Description: singly linked list node struct
+ * @next: points to the next node
+ * Description: singly linked list node struct.
  */
 typedef struct listint_s
 {
-int n;
-struct listint_s *next;
+    int n;
+    struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
